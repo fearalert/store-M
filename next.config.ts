ad7a5cd56934f7f20f75d99f95de23dee27f3319
Rohan Dhakal <67753397+fearalert@users.noreply.github.com>
2025-06-06 12:05:48 +0545

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // 👈 Required for static export
-
   typescript: {
     ignoreBuildErrors: true,
   },
